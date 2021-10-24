b89e272481f208473e0ad1668c500f1482da09c7
pedro <pedrohenriqueribeirodearaujo01@gmail.com>
2021-10-24 08:52:43 -0300

    Implementacao de processamento de contrato concluida

--- frontend/src/pages/Contratos/ContratosList/index.js
+++ frontend/src/pages/Contratos/ContratosList/index.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { SuccessButton } from '../../../components/Button';
+import { SuccessButton, DefaultButton } from '../../../components/Button';
 import api from '../../../services/api';
 import moment from 'moment';
 import plusIcon from '../../../assets/plus.svg';
+import doubleGearIcon from '../../../assets/double-gear.svg';
 import {SearchInput} from '../../../components/Input';
 import PaginationInput from '../../../components/PaginationInput';
 
 export default function ContratosList() {
     async function loadListAsync(page) {
         let { data } = await api
-            .get(`/contratos?page=${page}&search=${search}`);
+        .get(`/contratos?page=${page}&search=${search}`);
         setContratos(data.records);
         setNumberOfPages(data.numberOfPages);
     }
-
+    
+    async function processContrato(id) {
+        await api.post('/contratos/process/' + id);
+        await loadList(page);
+    }
     
     function loadList(page) {
         loadListAsync(page);
@@ -24,6 +29,7 @@ export default function ContratosList() {
         setSearch(value);
         setPage(1);
     }
+
 
     let [contratos, setContratos] = useState([]);
     let [page, setPage] = useState(1);
@@ -58,14 +64,30 @@ export default function ContratosList() {
                         </tr>
                     </thead>
                     <tbody>
-                    {contratos?.map(({id, cliente, plano, venc, countDependentes}) => (
+                    {contratos?.map(({
+                            id, 
+                            cliente, 
+                            plano, 
+                            venc, 
+                            countDependentes, 
+                            isProcessed
+                        }) => (
                         <tr>
                             <td>{id}</td>
                             <td>{cliente}</td>
                             <td>{plano}</td>
                             <td>{countDependentes}</td>
                             <td>{moment(venc).format('DD/MM/YYYY')}</td>
-                            <td></td>
+                            <td>
+                                {!isProcessed  ? (
+                                    <DefaultButton
+                                        icon={doubleGearIcon} 
+                                        onClick={() => processContrato(id)} />
+                                    
+                                ): (
+                                    <div></div>
+                                )}
+                            </td>
                         </tr>
                     ))}
                     </tbody>
